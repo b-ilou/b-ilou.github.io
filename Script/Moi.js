6377bca369da0898bf129fc909804159d2b8518e
b-ilou <bilougeek@gmail.com>
2024-06-22 16:02:48 +0200

    Moi

---
+++ Script/Moi.js
@@ -0,0 +1,102 @@
+function descente(){
+    document.getElementById('proj1').style.marginTop = '5px';
+}
+
+function remonte(){
+    document.getElementById('proj1').style.marginTop = '-30px';
+}
+
+function descente1(){
+    document.getElementById('proj2').style.marginTop = '5px';
+}
+
+function remonte1(){
+    document.getElementById('proj2').style.marginTop = '-30px';
+}
+
+function descente2(){
+    document.getElementById('proj3').style.marginTop = '5px';
+}
+
+function remonte2(){
+    document.getElementById('proj3').style.marginTop = '-30px';
+}
+
+function descente3(){
+    document.getElementById('proj4').style.marginTop = '5px';
+}
+
+function remonte3(){
+    document.getElementById('proj4').style.marginTop = '-30px';
+}
+
+function descente4(){
+    document.getElementById('proj5').style.marginTop = '5px';
+}
+
+function remonte4(){
+    document.getElementById('proj5').style.marginTop = '-30px';
+}
+
+function cvon(){
+    document.getElementById("CV").src="Image/Moi/CV.gif";
+}
+
+function cvoff(){
+    document.getElementById("CV").src="Image/Moi/CV.png";
+}
+
+function linkedinon(){
+    document.getElementById("LinkedIn").src="Image/Moi/LinkedIn.gif";
+}
+
+function linkedinoff(){
+    document.getElementById("LinkedIn").src="Image/Moi/LinkedIn.png";
+}
+
+function contacton(){
+    document.getElementById("Contact").src="Image/Moi/Contact.gif";
+}
+
+function contactoff(){
+    document.getElementById("Contact").src="Image/Moi/Contact.png";
+}
+
+function pluson(){
+    document.getElementById("Plus").src="Image/Accueil/plus.gif";
+}
+
+function plusoff(){
+    document.getElementById("Plus").src="Image/Accueil/Plus.png";
+}
+
+const textElement = document.querySelector('.Titre');
+
+function setGradientAngle(angle) {
+    textElement.style.setProperty('--gradient', `-webkit-linear-gradient(${angle}, #ffef0a 0%, #ffbb3e 30%, #ffee00 100%)`);
+}
+
+let increasing = true;
+let currentPercentage = 0;
+const step = 0.1; // Vous pouvez ajuster la vitesse en modifiant cette valeur
+
+function updateGradientAngle() {
+    if (increasing) {
+        currentPercentage += step;
+        if (currentPercentage >= 100) {
+            currentPercentage = 100;
+            increasing = false;
+        }
+    } else {
+        currentPercentage -= step;
+        if (currentPercentage <= 0) {
+            currentPercentage = 0;
+            increasing = true;
+        }
+    }
+
+    setGradientAngle(`${currentPercentage}deg`);
+    requestAnimationFrame(updateGradientAngle);
+}
+
+updateGradientAngle();
