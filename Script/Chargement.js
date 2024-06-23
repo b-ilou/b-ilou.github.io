8f6e50fae34c6d46ee8d707935fd44bddeb5abed
b-ilou <bilougeek@gmail.com>
2024-06-23 13:18:10 +0200

    Prefetch

--- Script/Chargement.js
+++ Script/Chargement.js
@@ -1,15 +1,19 @@
 window.onload = function() {
 
-    document.getElementById("animation").src="Image/Chargement.gif";
-
-    setTimeout(function() {
-        document.getElementById('chargement').style.opacity = '0';
-        document.getElementById("animation").src="Image/fondcouper.png";
-        document.getElementById('animation').style.display = 'none';
+    document.getElementById('chargement').style.opacity = '0';
+    
         setTimeout(function() {
             document.getElementById('chargement').style.display = 'none';
         }, 1000);
-        }, 4800);
 
+    // document.getElementById("animation").src="Image/Chargement.gif";
 
+    // setTimeout(function() {
+    //     document.getElementById('chargement').style.opacity = '0';
+    //     document.getElementById("animation").src="Image/fondcouper.png";
+    //     document.getElementById('animation').style.display = 'none';
+    //     setTimeout(function() {
+    //         document.getElementById('chargement').style.display = 'none';
+    //     }, 1000);
+    //     }, 4800);
 };
